efc2bc9780d586dfe424e60d9aff9605be6247b4
jaime <168496852+13852989@users.noreply.github.com>
2026-01-06 01:04:08 -0500

    ads-config.js

---
+++ ads-config.js
@@ -0,0 +1,8 @@
+window.ADS_CONFIG = {
+  links: [
+    "https://www.effectivegatecpm.com/f6c27aq0?key=5c96f8efeb08677d5e10077dfcfaea7c",
+    "https://www.effectivegatecpm.com/b3w4id77c?key=2456212f23dcb3b8390dba909a268719",
+    "https://www.effectivegatecpm.com/u0jcsy6r?key=47519f4b8082732b99f110faa972b481"
+  ],
+  maxPerSession: 2
+};
