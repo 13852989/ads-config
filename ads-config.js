daec24c159391ffb5fe0b0ce2ae0a189fac9708a
jaime <168496852+13852989@users.noreply.github.com>
2026-01-17 03:56:43 -0500

    Update ads-config.js

--- ads-config.js
+++ ads-config.js
@@ -1,8 +1,7 @@
-window.ADS_CONFIG = {
-  links: [
-    "https://www.effectivegatecpm.com/f6c27aq0?key=5c96f8efeb08677d5e10077dfcfaea7c",
-    "https://www.effectivegatecpm.com/b3w4id77c?key=2456212f23dcb3b8390dba909a268719",
-    "https://www.effectivegatecpm.com/u0jcsy6r?key=47519f4b8082732b99f110faa972b481"
-  ],
-  maxPerSession: 2
-};
+ADS_CONFIG = {
+  provider: "monetag",
+  format: "popunder",
+  zoneId: "10476753",
+  maxPerSession: 1,
+  devices: ["desktop"]
+}
