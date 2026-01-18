3f4d657b442150836846a0240c3acded20c260e7
jaime <168496852+13852989@users.noreply.github.com>
2026-01-18 00:27:54 -0500

    Update ads-config.js

--- ads-config.js
+++ ads-config.js
@@ -1,37 +1,11 @@
-(function () {
+window.ADS_CONFIG = {
+  links: [
+    "https://otieu.com/4/10476863",
+    "https://otieu.com/4/10480476",
+    "https://otieu.com/4/10480477",
+    "https://otieu.com/4/10480478"
+  ],
+  maxPerSession: 1
+};
 
-  const ADS_CONFIG = {
-    provider: "monetag",
-    format: "popunder",
-    zoneId: "10476753",
-    maxPerSession: 1
-  };
-
-  function getCount() {
-    return Number(sessionStorage.getItem("monetag_count") || 0);
-  }
-
-  function incCount() {
-    sessionStorage.setItem("monetag_count", getCount() + 1);
-  }
-
-  window.runAds = function () {
-    if (getCount() >= ADS_CONFIG.maxPerSession) return;
-
-    // 🔥 ventana fantasma (CLAVE)
-    const win = window.open("about:blank", "_blank");
-    if (!win) return;
-
-    // inyectar Monetag
-    const s = document.createElement("script");
-    s.src = "https://al5sm.com/tag.min.js";
-    s.dataset.zone = ADS_CONFIG.zoneId;
-    s.async = true;
-
-    win.document.body.appendChild(s);
-
-    incCount();
-  };
-
-})();
 
